5eedb52af5d3c55dd0d1a3395d9e154ccd17ec90
Nick Bradley <nick.bradley.193@pm.me>
2018-04-25 20:04:44 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -4,7 +4,10 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
-  let index = 0
+  let index = 0;
+  
+  document.body.addEventListener('keydown', listen(event));
+  
+  
 }
 
-document.body.addEventListener('keydown', init(e))
